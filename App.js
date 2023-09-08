e698c7ee6b260a11d80e24912cb0a6b36dfd3933
Caleb Hatch <hatchcaleb@gmail.com>
2023-09-07 19:51:24 -0500

    got .env file finally working

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { ActivityIndicator, View, StyleSheet } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native'
 import Tabs from './src/components/Tabs'
 import * as Location from 'expo-location'
-// import { TEST_KEY } from 'react-native-dotenv'
+import { TEST_KEY } from 'react-native-dotenv'
 
 // api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
 
@@ -12,10 +12,9 @@ const App = () => {
   const [loading, setLoading] = useState(true)
   const [location, setLocation] = useState(null)
   const [error, setError] = useState(null)
-
-  // console.log(TEST_KEY);
+  console.log(TEST_KEY);
   
-
+  
   useEffect(() => {
     (async () => {
       let { status } = await Location.requestForegroundPermissionsAsync()
